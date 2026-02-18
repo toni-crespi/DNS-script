35b47a7775a3875358a894e885710f43c9a4c757
toni-crespi <tonicrespi@paucasesnovescifp.cat>
2026-02-18 01:22:43 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,10 +7,10 @@ mkdir -p /etc/bind/zones
 echo "=== Descarregant fitxers de configuració des de GitHub ==="
 
 # Descarreguem zona botson.org
-wget https://raw.githubusercontent.com/toni-crespi/DNS-script/main/db.botson.org -O /etc/bind/zones/db.botson.org
+wget https://raw.githubusercontent.com/toni-crespi/DNS-script/main/db.botson.com -O /etc/bind/zones/db.botson.com
 
 # Descarreguem zona estacions (Compte: al teu GitHub es diu 'db.estacionsbotson.org')
-wget https://raw.githubusercontent.com/toni-crespi/DNS-script/main/db.estacionsbotson.org -O /etc/bind/zones/db.estacions.botson.org
+wget https://raw.githubusercontent.com/toni-crespi/DNS-script/main/db.estacionsbotson.com -O /etc/bind/zones/db.estacions.botson.com
 
 # Descarreguem zona inversa (Compte: al teu GitHub es diu 'db.zonainversa')
 wget https://raw.githubusercontent.com/toni-crespi/DNS-script/main/db.zonainversa -O /etc/bind/zones/db.50.18.10
@@ -20,8 +20,8 @@ wget https://raw.githubusercontent.com/toni-crespi/DNS-script/main/named.conf.lo
 
 echo "=== Comprovant sintaxi ==="
 named-checkconf
-named-checkzone botson.org /etc/bind/zones/db.botson.org
-named-checkzone estacions.botson.org /etc/bind/zones/db.estacions.botson.org
+named-checkzone botson.com /etc/bind/zones/db.botson.com
+named-checkzone estacions.botson.com /etc/bind/zones/db.estacions.botson.com
 named-checkzone 50.18.10.in-addr.arpa /etc/bind/zones/db.50.18.10
 
 echo "=== Reiniciant Bind9 ==="
